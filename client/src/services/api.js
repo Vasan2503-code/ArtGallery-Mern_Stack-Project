38390010de5d84932fcb0e77c10ef6931ffe0fe4
Vasan2503-code <srinivasansiet142@gmail.com>
2026-02-11 09:29:22 +0530

    url changed

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create axios instance
 const api = axios.create({
-    baseURL: "http://localhost:4000",
+    baseURL: "https://artgallery-y0rw.onrender.com",
     headers: {
         "Content-Type": "application/json",
     },
